11418c48943ea16eddc1b8d66b62ea5915814268
LeonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2026-04-04 22:20:12 -0300

    ajustando os pesos para meu exemplo de escolha de filmes

--- perceptron_3.py
+++ perceptron_3.py
@@ -1,23 +1,27 @@
 import numpy as np
 
+
+# Dados de entrada: nota do filme, compatibilidade de gênero
 entradas = np.array([
-    [1, 1],
-    [2, 1],
-    [3, 2],
-    [4, 1]
+    [1, 1],  # Filme A
+    [2, 1],  # Filme B
+    [3, 2],  # Filme C       
+    [4, 1]   # Filme D    
 ])
 
+# w1 = peso da nota  w2 = peso da compatibilidade de genero
+pesos = np.array([0.1, 0.4])
 
-pesos = np.array([0.2, 0.3])
 
 def soma(e, p):
     return e.dot(p)
 
 def stepFunction(s):
-    return (s >= 0.5).astype(int)
+    return (s > 0.5).astype(int)
 
 s = soma(entradas, pesos)
 r = stepFunction(s)
+
 
 print(f"somas:    {s}")
 print(f"saidas:   {r}")
